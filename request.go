8cf342b0c69d9f709d02185829e708a1c9ec8724
Aleksander Piotrowski <apiotrowski312@gmail.com>
2019-10-28 18:59:18 +0100

    add Post

--- request.go
+++ request.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"time"
 
@@ -9,8 +10,25 @@ import (
 
 // Get - simple function to Get url and collect data
 func Get(url string) results.Result {
+	get := func() (*http.Response, error) {
+		return http.Get(url)
+	}
+
+	return request(get)
+}
+
+// Post - simple function to Get url and collect data
+func Post(url string, byteType string, bytes io.Reader) results.Result {
+	post := func() (*http.Response, error) {
+		return http.Post(url, byteType, bytes)
+	}
+
+	return request(post)
+}
+
+func request(method func() (*http.Response, error)) results.Result {
 	start := time.Now()
-	resp, err := http.Get(url)
+	resp, err := method()
 
 	if err != nil {
 		return results.CreateResult(time.Since(start), 500)
